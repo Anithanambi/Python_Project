7e1c5bba595e30ff743360c3ab425f9b5360ea6d
Anitha <anitha.venkat83@gmail.com>
2022-07-04 15:14:18 -0400

    Testing the Commit
    exit

--- Addition.py
+++ Addition.py
@@ -2,12 +2,21 @@
 # coding: utf-8
 
 # In[1]:
-
-
-print("Hello World")
-
-
+print("This is Addition File")
+N1=input("Enter Number 1")
+N2=input("Eter Number 2")
+print("Result of addition is =", N1+N2)
 # In[ ]:
+
+
+
+
+
+
+
+
+
+
 
 
 
