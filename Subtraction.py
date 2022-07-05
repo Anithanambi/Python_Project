c4fabc03f45c25030586df30e528fcc2574ecb2e
Anitha <anitha.venkat83@gmail.com>
2022-07-04 21:26:31 -0400

    first commit

---
+++ Subtraction.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+print("This is Addition File")
+N1=input("Enter Number 1")
+N2=input("Eter Number 2")
+print("Result of addition is =", N1+N2)
+# In[ ]:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
